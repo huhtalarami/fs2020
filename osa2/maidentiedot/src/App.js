8abbc88ce697979bf72c8429b2f1c239e28c3944
Rami Huhtala <rami.huhtala@digia.com>
2020-03-31 08:50:11 +0300

    lisätty säätiedot

--- osa2/maidentiedot/src/App.js
+++ osa2/maidentiedot/src/App.js
@@ -4,17 +4,13 @@ import CountriesList from './components/CountriesList'
 import Country from './components/Country'
 
 const  App = () => {
-
   const [ countries, setCountries] = useState([])
   const [ newFilter, setNewFilter ] = useState('') 
   const [ countriesToShow, setCountriesToShow ] = useState([])
 
-
   useEffect(() => {
-    console.log('effect')
     axios
       .get('https://restcountries.eu/rest/v2/all').then(response => {
-        console.log('promise fulfilled')
         setCountries(response.data)
       })
   }, [])
--- osa2/maidentiedot/src/components/Country.js
+++ osa2/maidentiedot/src/components/Country.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect  } from 'react'
+import axios from 'axios'
 
 const Country = ({countryToShow}) => {
+  const api_key = process.env.REACT_APP_API_KEY
+  const [ wheather, setWheather] = useState([])
+
+  useEffect(() => {
+    axios
+      .get(`https://api.weatherstack.com/current?access_key = ${api_key} & query = ${countryToShow.name}`).then(response => {
+        setWheather(response.data)
+      })
+  }, [])
+
   return (
     <div>
       <h1>{countryToShow.name} </h1>
@@ -13,10 +24,12 @@ const Country = ({countryToShow}) => {
       }
       </ul>
       <img src={countryToShow.flag} width="150" height="100" alt="flag"/>
-    <h3>Weather in {countryToShow.capital}</h3>
-    Always sunshine
+      <h3>Weather in {countryToShow.capital}</h3>
+        temperature {wheather.current.temperature}
+        {wheather.weather_icons}
+        wind {wheather.current.wind_speed}
     </div>
-   )
+  )
 }    
 
 export default Country
